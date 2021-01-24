72534afb6ac5c90188e0344e2d4b7d2b25683e35
Karan Manglani <bkn45for@gmail.com>
2021-01-24 08:01:59 +0530

    Started Understanding tkinter in deep

--- 1. Meeting Tkinter/1. rootWindow.py
+++ 1. Meeting Tkinter/1. rootWindow.py
@@ -5,5 +5,5 @@ label = tk.Label(root, text="I am a label widget")
 button = tk.Button(root, text="I am a button")
 label.pack()
 button.pack()
-
+tk.Label(root,text='Direct Addition into the application (Use only if you dont need to manipulate or retrieve data from this labal)').pack()
 root.mainloop()
